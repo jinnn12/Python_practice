5c68be4fb967be0bb23954c22863b08b83a43528
Jinho Kim <prohh12@naver.com>
2026-02-23 18:12:40 +0900

    __all__

--- game/__init__.py
+++ game/__init__.py
@@ -4,3 +4,6 @@ VERSION = 3.10
 
 def print_version_info():
     print(f"The version of this game is {VERSION}")
+
+# 패키지 초기화 코드 작성
+print("Initializing game ...")
---
+++ game/sound/__init__.py
@@ -0,0 +1,2 @@
+# __all__은 sound 디렉터리에서 *를 사용하여 import 할 경우, 이곳에 정의된 echo 모듈만 import 된다는 의미이다.
+__all__ = ['echo']
